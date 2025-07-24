417c83c19e1f8977aca234898a42b0fff518ec45
Jorge Rodriguez <lilsaintdj@gmail.com>
2025-07-24 10:25:27 -0600

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1753374323110

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_e50b23a8._.js",
+  "static/chunks/src_fb5f3fce._.js"
+],
+    source: "dynamic"
+});
